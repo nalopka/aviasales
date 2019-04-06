46bbd6bdcb4f44fad049eb7d8abcc8703d445214
Evgeny <nalopka@gmail.com>
2019-04-06 13:29:01 +0700

    eject settings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import axios from 'axios';
+import * as _ from 'lodash';
 import React, { Component } from 'react';
+import Ticket from 'src/components/Ticket';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>Test</p>
-      </div>
-    );
-  }
+    state = {
+        tickets: [],
+    };
+
+    componentDidMount() {
+        this.fetchTickets();
+    }
+
+    fetchTickets = () => {
+        axios.get('/tickets.json')
+            .then(function(response) {
+                const tickets = _.result(response, 'data.tickets', {});
+                this.setState({tickets})
+            })
+            .catch(function(error) {
+                console.log(error);
+            })
+    };
+
+    render() {
+        const { tickets } = this.state;
+        return (
+            <div className="App">
+                <Ticket
+                    listOfTickets={tickets}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Ticket/Ticket.jsx
@@ -0,0 +1,12 @@
+import * as _ from 'lodash';
+import React from 'react';
+import './App.css';
+
+function Ticket({tickets}) {
+    // console.log(ticke
+    return (
+        <h1>Hello</h1>
+    );
+}
+
+export default Ticket;
